c3214e868a5e4df374c6b3c1f541f15e483d9966
Kozaev Dzambolat <otrina1@yandex.ru>
2020-02-22 13:41:33 +0300

    Сделанные while циклы

---
+++ while1.py
@@ -0,0 +1,21 @@
+"""
+
+Домашнее задание №1
+
+Цикл while: ask_user
+
+* Напишите функцию ask_user(), которая с помощью input() спрашивает 
+  пользователя “Как дела?”, пока он не ответит “Хорошо”
+   
+"""
+
+
+def ask_user():
+  while True:
+    user_input = input('Как дела? ')
+    if user_input == 'Хорошо':
+      break
+
+    
+if __name__ == "__main__":
+    ask_user()
